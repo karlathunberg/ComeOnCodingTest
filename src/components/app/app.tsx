816c81cf194e89283c86e4e31f6aa960e150da92
Karl A-Thunberg <karlathunberg@outlook.com>
2020-09-20 22:11:25 +0200

    Redirect users to login page if navigating to pages that require a logged in user

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,13 @@
 import * as React from 'react'
-import { Provider } from 'react-redux'
-import { Redirect, Route, Router as RouterDom, Switch } from 'react-router-dom'
+import { Provider, useSelector } from 'react-redux'
+import {
+  Redirect,
+  Route,
+  RouteComponentProps,
+  RouteProps,
+  Router as RouterDom,
+  Switch,
+} from 'react-router-dom'
 
 import { LocalizationProvider } from '../../localization'
 import configureStore from '../../redux/configure-store'
@@ -9,8 +16,44 @@ import LoginPage from '../login-page'
 import GamesPage from '../games-page'
 import GamePage from '../game-page'
 import NotFoundPage from '../not-found-page'
+import { selectAuthState } from '../../redux/auth'
 
 const store = configureStore()
+
+interface IProps extends RouteProps {
+  component?: React.ComponentType<RouteComponentProps>
+}
+
+const AuthenticatedRoute: React.FunctionComponent<IProps> = ({
+  component: Component,
+  children,
+  ...rest
+}) => {
+  const { isAuthenticated, isLoading } = useSelector(selectAuthState)
+
+  const handleRender = React.useCallback(
+    (props) => {
+      if (isLoading) {
+        // In case render gets ahead of the log in flow
+        return <div />
+      } else if (!isAuthenticated) {
+        // not logged in so redirect to login page
+        return (
+          <Redirect
+            to={{
+              pathname: '/login',
+            }}
+          />
+        )
+      }
+
+      return Component ? <Component {...props} /> : children
+    },
+    [Component, children, isLoading, isAuthenticated]
+  )
+
+  return <Route {...rest} render={handleRender} />
+}
 
 const AppContent = () => (
   <RouterDom history={history}>
@@ -19,8 +62,8 @@ const AppContent = () => (
         <Redirect to="/login" />
       </Route>
       <Route exact={true} path="/login" component={LoginPage} />
-      <Route exact={true} path="/games" component={GamesPage} />
-      <Route exact={true} path="/game" component={GamePage} />
+      <AuthenticatedRoute exact={true} path="/games" component={GamesPage} />
+      <AuthenticatedRoute exact={true} path="/game" component={GamePage} />
       <Route path="/" component={NotFoundPage} />
     </Switch>
   </RouterDom>
