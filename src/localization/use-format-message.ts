c52e6f113701dec1ad8a81257796680670a6aa31
Karl A-Thunberg <karlathunberg@outlook.com>
2020-09-23 21:58:00 +0200

    Split localization hook return value to one for text and one for react components Since some texts can only be strings (alt, placeholders, etc)

--- src/localization/use-format-message.ts
+++ src/localization/use-format-message.ts
@@ -1,11 +1,28 @@
-import { PrimitiveType } from 'intl-messageformat'
+import { FormatXMLElementFn, PrimitiveType } from 'intl-messageformat'
 import { useIntl } from 'react-intl'
 import { IMessages } from './messages'
 
-export default () => {
+export default (): [
+  (
+    id: keyof IMessages,
+    values?: Record<string, FormatXMLElementFn<string, string>>
+  ) => string,
+  (
+    id: keyof IMessages,
+    values?: Record<string, PrimitiveType | React.ReactElement>
+  ) => React.ReactNode
+] => {
   const intl = useIntl()
-  return (
+
+  const formatText = (
+    id: keyof IMessages,
+    values: Record<string, FormatXMLElementFn<string, string>> = {}
+  ) => intl.formatMessage({ id }, values)
+
+  const formatComponent = (
     id: keyof IMessages,
     values: Record<string, PrimitiveType | React.ReactElement> = {}
   ) => intl.formatMessage({ id }, values)
+
+  return [formatText, formatComponent]
 }
